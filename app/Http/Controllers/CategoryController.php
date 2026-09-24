7aa49a1bfddf29fcc7bbdce4ec509b11e567adfd
Megyeri Márk Máté <megyko1234@gmail.com>
2026-09-24 16:41:43 +0200

    Tesztelés postmannal

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Category::orderBy('sort_order')->get(), 200);
+    }
 }
--- database/migrations/2026_09_24_143652_create_categories_table.php
+++ database/migrations/2026_09_24_143652_create_categories_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('sort_order')->default(0);
             $table->timestamps();
         });
     }
