750da7ef81ef1d2a29e549a0309d1d70cea5f1a1
Megyeri Márk Máté <megyko1234@gmail.com>
2026-09-24 18:01:58 +0200

    Regisztráció, bejelntkezés megírása

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $validated = $reguest->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8'
+        ],
+        ([
+        "name" => "Név kötelező",
+        "email" => "Email",
+        "password" => "minimum 8 karakter",
+        ]));
+        $user = User::create([
+            'name'=>$validated['name'],
+            'email'=>$validated['email'],
+            "password"=>Hash::make($validated["password"]),
+        ]);
+
+        $token = $user->createToken("auth_token")->plainTextToken;
+        return response()->json([
+            'message' => 'Sikeres regisztráció!',
+            'user' => $user,
+            'access_token' => $token,
+        ], 201);
+    }
+
+    public function login(Request $request){
+        $reguest->validate([
+            
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+            ],
+            ([
+        "email" => "email kötelező",
+        "password" => "jelszó kötelező",
+        ]));
+
+        $user = User::where('email', $request->email)->first();
+
+        //Jelszó ellenőrzése
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Hibás email vagy jelszó!'
+            ], 401); // 401 = Unauthorized (Jogosulatlan)
+        }
+
+        // 4. Új token generálása
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Sikeres bejelentkezés!',
+            'user' => $user,
+            'access_token' => $token,
+        ], 200);
+}
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +9,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get("/kategoriak", [CategoryController::class, "index"]);
+Route::post("/regisztracio", [AuthController::class, "register"]);
